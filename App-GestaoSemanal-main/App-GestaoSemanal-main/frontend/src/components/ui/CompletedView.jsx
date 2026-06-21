a792fb1c96228e13af96d44c4a8984fabd4f8cfb
AppGeSmnl <gestaosemanalapp@gmail.com>
2026-06-21 18:40:28 -0300

    Update CompletedView.jsx

--- App-GestaoSemanal-main/App-GestaoSemanal-main/frontend/src/components/ui/CompletedView.jsx
+++ App-GestaoSemanal-main/App-GestaoSemanal-main/frontend/src/components/ui/CompletedView.jsx
@@ -421,13 +421,12 @@ for (let y = 2024; y <= 2030; y++) {
 </div>
 
 {/* MÊS */}
-<div className="flex items-center gap-2">
+<div className="flex items-center gap-2 relative">
   <label className="text-sm text-white font-medium">
     Mês:
   </label>
 
-  <div className="flex gap-2">
-
+  
 <Select
   value={selectedMonth}
   onValueChange={setSelectedMonth}
