6580b6c565d9d6b07a46815ae53ae15fa8b8b5c6
AppGeSmnl <gestaosemanalapp@gmail.com>
2026-06-20 22:06:59 -0300

    Update App.js

--- App-GestaoSemanal-main/App-GestaoSemanal-main/frontend/src/App.js
+++ App-GestaoSemanal-main/App-GestaoSemanal-main/frontend/src/App.js
@@ -327,7 +327,7 @@ function PresentationMode({ demands, categoryTitle, onClose, singleDemand, onUpd
                 </div>
               </div>
             ) : (
-            <div className="h-[250px] overflow-y-auto pr-4">
+            <div className="h-[200px] overflow-y-auto pr-4">
               <p className="text-2xl md:text-3xl text-slate-400 font-medium leading-relaxed italic whitespace-pre-wrap">
                 {currentDemand.observation ? `${currentDemand.observation}` : "Nenhuma observação."}
               </p>
