ebafba0cf77c7a251efda7d3ee52a5b0f1423d39
AppGeSmnl <gestaosemanalapp@gmail.com>
2026-06-21 13:11:18 -0300

    Update AdminView.jsx

--- App-GestaoSemanal-main/App-GestaoSemanal-main/frontend/src/components/ui/AdminView.jsx
+++ App-GestaoSemanal-main/App-GestaoSemanal-main/frontend/src/components/ui/AdminView.jsx
@@ -119,7 +119,7 @@ const deleteSubgroup = async (id) => {
     <div className="max-w-7xl mx-auto px-6 py-8">
 
       <h1 className="text-3xl font-bold mb-8">
-        Administração
+        Equipe
       </h1>
 
       <div className="grid grid-cols-2 gap-8">
